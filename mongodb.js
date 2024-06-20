f7b1ea516c22138c99f5e8d1d79effd82a0b2ce9
Shivam24S <shivamshrivastav8055@gmail.com>
2024-06-21 00:21:53 +0530

    inserting many

--- mongodb.js
+++ mongodb.js
@@ -11,11 +11,33 @@ const database = "task-manager";
 async function addUser() {
   const db = (await client).db(database);
 
-  const user = await db.collection("Users").insertOne({
-    name: "shivam shrivastav",
-    age: 22,
-  });
-  console.log(user);
+  // const user = await db.collection("Users").insertOne(
+  //   {
+  //     name: "shivam shrivastav",
+  //     age: 22,
+  //   },
+  //   (err, result) => {
+  //     if (err) {
+  //       console.log("can't able to add user");
+  //     } else {
+  //       console.log(result.ops);
+  //     }
+  //   }
+  // );
+  // console.log(user);
+
+  // insertMany
+
+  const users = await db.collection("Users").insertMany([
+    {
+      name: "shivay",
+      age: 20,
+    },
+    {
+      name: "shiva",
+      age: 23,
+    },
+  ]);
 }
 
 addUser();
