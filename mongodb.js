9129fc32e419a50c46598caa4338f89407fe55df
Shivam24S <shivamshrivastav8055@gmail.com>
2024-06-22 02:16:38 +0530

    updating One

--- mongodb.js
+++ mongodb.js
@@ -48,40 +48,73 @@
 
 // reading from database
 
+// const mongodb = require("mongodb");
+
+// const connectionURL = "mongodb://127.0.0.1:27017";
+
+// const database = "task-manager";
+
+// const client = mongodb.MongoClient.connect(connectionURL);
+
+// async function readingData() {
+//   try {
+//     const db = (await client).db(database);
+
+//     // findOne it will return only first match
+//     // const userData = await db.collection("Users").findOne({ name: "shivay" });
+
+//     // find it will return an array from db who all match condition
+
+//     const userData = await db
+//       .collection("Users")
+//       .find({ age: 22 })
+//       .toArray((err, users) => {
+//         if (err) {
+//           return console.log("no match found");
+//         } else return users;
+//       });
+
+//     if (!userData) {
+//       return console.log("No user found");
+//     } else {
+//       console.log(userData);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// readingData();
+
+// updating in database
+
 const mongodb = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 
-const database = "task-manager";
-
 const client = mongodb.MongoClient.connect(connectionURL);
 
-async function readingData() {
+const database = "task-manager";
+
+const ObjectId = mongodb.ObjectId;
+
+async function updatingData() {
   try {
     const db = (await client).db(database);
 
-    // findOne
-    // const userData = await db.collection("Users").findOne({ name: "shivay" });
-
-    // find
-
-    const userData = await db
-      .collection("Users")
-      .find({ age: 22 })
-      .toArray((err, users) => {
-        if (err) {
-          return console.log("no match found");
-        } else return users;
-      });
-
-    if (!userData) {
-      return console.log("No user found");
-    } else {
-      console.log(userData);
-    }
+    db.collection("Users").updateOne(
+      {
+        _id: new ObjectId("667479f2b5f0c3f1952e3d9e"),
+      },
+      {
+        $set: {
+          name: "updated name",
+        },
+      }
+    );
   } catch (error) {
     console.log(error);
   }
 }
 
-readingData();
+updatingData();
