db8cd8f33b7fd9ab1f8814c33d469343fe366b74
Shivam24S <shivamshrivastav8055@gmail.com>
2024-06-27 12:34:58 +0530

    completing challenge using async await

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,25 +1,59 @@
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+//   //   useNewUrlParser: true,
+//   //   useCreateIndex: true,
+// });
+
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//   },
+//   age: {
+//     type: Number,
+//   },
+// });
+
+// const userDetails = new User({
+//   name: "shivam",
+//   age: 22,
+// });
+
+// userDetails
+//   .save()
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+// challenge
+
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
-  //   useNewUrlParser: true,
-  //   useCreateIndex: true,
-});
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
 
-const User = mongoose.model("User", {
-  name: {
+const task = mongoose.model("Task-data", {
+  description: {
     type: String,
   },
-  age: {
-    type: Number,
+  completed: {
+    type: Boolean,
   },
 });
 
-const userDetails = new User({
-  name: "shivam",
-  age: 22,
+const taskData = new task({
+  description: "learning",
+  completed: true,
 });
 
-userDetails
-  .save()
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+async function addingTask() {
+  try {
+    const taskInputs = taskData.save();
+
+    const result = await taskInputs;
+
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+addingTask();
